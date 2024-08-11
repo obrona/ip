966840a000228d14ac93143244efef1b2ed7dd03
obrona <theodorelim2002@gmail.com>
2024-08-11 15:20:02 +0800

    Level-2

--- src/main/java/Duch.java
+++ src/main/java/Duch.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duch {
+    static Printer p = new Printer();
     public static void main(String[] args) {
+        ArrayList<String> texts = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, I am Duch.\nWhat can I do for you?");
         while (true) {
-            String echo = scanner.nextLine();
-            if (echo.equals("bye")) break;
-            System.out.println(echo);
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
+                break;
+            } else if (text.equals("list")) {
+                String str = "";
+                for (int i = 0; i < texts.size(); i ++) {
+                    str += (i + 1) + ": " + texts.get(i) + "\n";
+                }
+                p.print(str);
+            } else {
+                texts.add(text);
+                p.print("added: " + text + '\n');
+            }
         }
+        scanner.close();
         System.out.println("Bye. Hope to see you again");
     }
 }
