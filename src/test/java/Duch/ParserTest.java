8665a3e47b8eea706e5bb48bf0063e1d20065ea5
obrona <theodorelim2002@gmail.com>
2024-08-25 14:05:17 +0800

    A-Jar: Create a JAR File

---
+++ src/test/java/Duch/ParserTest.java
@@ -0,0 +1,25 @@
+package Duch;
+
+import org.junit.jupiter.api.Test;
+
+import Duch.Task.Task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParserTest {
+    class TaskListStub extends TaskList {
+        TaskListStub() {
+            super();
+        }
+    }
+    
+    
+    
+    @Test
+    public void testConcat() {
+        Parser parser = new Parser(new TaskListStub());
+        String[] arr = {"a1", "a2", "a3", "a4", "a5"};
+        String expected = "a2 a3";
+        assertEquals(expected, parser.concat(arr, 1, 3));
+    }
+}
