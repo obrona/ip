613cec1fa4377a5479360eae9bcceefa078f9085
obrona <theodorelim2002@gmail.com>
2024-09-10 13:23:52 +0800

    Untrack .vscode

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,8 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java ../src/main/java/Task/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java 
+    ../src/main/java/GUI/*.java ../src/main/java/Duch/Task/*.java ../src/main/java/Duch/History/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
